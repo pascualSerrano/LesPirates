7f7f15a866fc8287d68ffe4148af3e3ecef4b5f4
pascualSerrano <pascual.asio-serrano@univ-tlse3.fr>
2025-03-23 13:07:01 +0100

    Diagrammes de Conception POO format pdf

--- src/cartes/AbordageReussi.java
+++ src/cartes/AbordageReussi.java
@@ -14,6 +14,7 @@ public class AbordageReussi extends Carte {
     @Override
     public void effetSpecial(Joueur joueur, Joueur adversaire) {
         appliquerEffet(joueur, adversaire, 2, -2, -1,-3);
+        //appliquerEffet(joueur, adversaire, PopJoueur, PopAdversaire, VieJoueur, VieAdversaire)
         affichage.afficherAbordageReussi(joueur, adversaire);
     }
 }
